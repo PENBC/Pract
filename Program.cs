47e214dc96a319cfc11adbdd272ed1e7f1f09ff0
PENBC <e007ky124@gmail.com>
2026-09-17 13:01:36 +0700

    1.2

--- Program.cs
+++ Program.cs
@@ -1,34 +1,14 @@
-﻿using System.Linq;
-using System.Text.RegularExpressions;
-namespace ConsoleApp3
+﻿internal class Programm
 {
-    internal class Program
+    public class ConsoleReport<T>(List<T> args)
     {
-        public class MenuObject(TimeOnly time, double price, string name)
+        public void Describe()
         {
-            public TimeOnly Time {
-                get { return time; }
-                set { time = value; }
-            }
-            public double Price {
-                get { return price; }
-                set { price = value; }
-            }
-            public string Name {
-                get { return name;  }
-                set { name = value; }
-            }
         }
-        static void Main(string[] args)
-        {
-            string Line = Console.ReadLine()?.Trim();
-            string name = Convert.ToString(Regex.Match(Line, @"""([^""]*)"""));
-            string price = Convert.ToString(Regex.Match(Line, @"\d+,\d+"));
-            string time = Convert.ToString(Regex.Match(Line, @"\d+:\d+"));
-            MenuObject newobject = new MenuObject(TimeOnly.Parse(time), Convert.ToDouble(price), name);
-            Console.WriteLine(newobject.Name);
-            Console.WriteLine(newobject.Price);
-            Console.WriteLine(newobject.Time);
-        }
+    }
+    public static void Main (string[] args)
+    {
+        List<string> words = new List<string> { "booba", "dooba", "gooba" };
+        ConsoleReport<string> test = new ConsoleReport<string>(words);
     }
 }
