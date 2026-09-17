7920791a5b5a72a589cc901901a2a0810ba2f370
PENBC <e007ky124@gmail.com>
2026-09-17 15:16:21 +0700

    задание с пятерками

---
+++ Program2.cs
@@ -0,0 +1,27 @@
+﻿public class Calc()
+{
+    public int CountFives(List<int> numbers)
+    {
+        int answer = 0;
+        foreach (int i in numbers)
+        {
+            answer += FoundFives(i);
+        }
+        return answer;
+    }
+    public int FoundFives(int number)
+    {
+        if (number % 10 == 5)
+        {
+            while (number < 10)
+            {
+                number = number % 10;
+            }
+            if (number == 5)
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
